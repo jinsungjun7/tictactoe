f0e757fbf8380d3baeecf0bf3a0cee6bfbbee820
jinsungjun7 <jun.james.paul@gmail.com>
2022-10-17 22:19:55 -0500

    Completed Javascript logic for 3 in a row and displaying messages for a winner

--- script.js
+++ script.js
@@ -1,13 +1,30 @@
+//factories
+const Player = ([playerType]) => {
+    let getPlayerType = () => playerType;
+    let playerScore = 0;
+    
+    function getPlayerScore() {
+       return playerScore;
+    }
+   
+    
+   
+    return {getPlayerScore,
+            getPlayerType,
+           };
+   
+   };
+
 //modules 
 const gameboard = (() => {
  let _gameboardArray = new Array(9);
 
- let p1Array = new Array();
- let p2Array = new Array();
+ let p1Array = [];
+ let p2Array = [];
  const _gameboardDiv = document.querySelector('div.gameboard');
- const _winningPattern = [['0','1','2'], ['0','4','8'], ['0','3','6'], 
-                          ['3','4','5'], ['1','4','7'], ['2','5','8'],
-                          ['2','4','6'], ['6','7','8']
+ const _winningPattern = [['n0','n1','n2'], ['n0','n4','n8'], ['n0','n3','n6'], 
+                          ['n3','n4','n5'], ['n1','n4','n7'], ['n2','n5','n8'],
+                          ['n2','n4','n6'], ['n6','n7','n8']
                         ];
  let gameOver = false;
  let playerTurn = 1;
@@ -16,7 +33,7 @@ const gameboard = (() => {
   if (gameOver) {
    _gameboardArray = new Array(9);
    _gameboardDiv .innerHTML = "";
-   toggleGameOver();
+   gameOver=false;
   }
 
   for (let i=0 ; i < _gameboardArray.length; i++) {
@@ -28,60 +45,68 @@ const gameboard = (() => {
   }
  };
 
- const setPiece = (index) => {
-  let numIndex = index.split('')[1]; 
-
-  if (_gameboardArray[numIndex] != null) {
-   return;
+ const setPiece = (numIndex) => {
+  let index = numIndex.split('')[1];
+  if (_gameboardArray[index] != null) {
+    return;
   } else if (playerTurn%2) {
-    _gameboardArray[numIndex] = 'O';
+    _gameboardArray[index] = 'O';
     p1Array.push(numIndex);
   } else {
-    _gameboardArray[numIndex] = 'X';
+    _gameboardArray[index] = 'X';
     p2Array.push(numIndex);
   }
 
-  let cell = document.querySelector(`#n${numIndex}`);
-  cell.textContent = _gameboardArray[numIndex];
+  let cell = document.querySelector(`#${numIndex}`);
+  cell.textContent = _gameboardArray[index];
+
   playerTurn++;
-  if (playerTurn >= 5) {
+  if (playerTurn > 5) {
     _checkGameboard();
   }
+  
  };
  
 
  const _checkGameboard = () => {
-  _winningPattern.every(arr => {
-   if (_isTrue(p1Array, arr)) {
-    alert("PLAYER 1 WINS!");
-    toggleGameOver();
-   } else if (_isTrue(p2Array, arr)) {
-    alert("PLAYER 2 WINS!");
-    toggleGameOver();
-   } else if (playerTurn > 9) {
-    alert("IT'S A TIE!");
-    toggleGameOver();
-   } else {
-    return;
+  let count = 0; 
+  while (!gameOver && count < 8) {
+    let arr = _winningPattern[count];
+    if (_isTrue(p1Array, arr)) {
+      displayController.displayWinner(1);
+      displayController.deactivateGameboard();
+      gameOver = true;
+     } else if (_isTrue(p2Array, arr)) {
+      displayController.displayWinner(2);
+      displayController.deactivateGameboard();
+      gameOver = true;
+     } 
+     count++;
+  };
+
+  if (playerTurn > 9 && !gameOver) {
+    displayController.displayWinner(0);
+    displayController.deactivateGameboard();
+    gameOver = true;
    }
-  });
  }; 
 
  function _isTrue(playerArray, winningArray) {
     return winningArray.every(r => playerArray.includes(r));
   };
-
- const getGameboard = () => {
-    return _gameboardArray;
- }
  
  const toggleGameOver = () => {
-    gameOver = !gameOver;
+    if (!gameOver) {
+      gameOver = !gameOver;
+    };
+    p1Array = [];
+    p2Array = [];
+    playerTurn = 1;
  }
+
 
  return {createGameboard, 
          setPiece,
-         getGameboard,
          toggleGameOver,
         };
 
@@ -90,30 +115,69 @@ const gameboard = (() => {
 const displayController = (() => {
  //add listener for New Game btn
  const startGameBtn = document.querySelector('.resetBtn');
+
  const startGame = () => {
   startGameBtn.addEventListener('click', (e) => {
     gameboard.toggleGameOver();
     gameboard.createGameboard();
     activateGameboard();
+    _hideMessages();
   })};
+
+ function togglePlayerType(buttonVal) {
+    //toggle for playing against a computer
+    //buttonVal.value = (buttonVal.value=="Player")?"Computer":"Player";
+ }
 
  //add listener for clicking a cell on the gameboard
  const activateGameboard = () => {
   let gameboardCells = document.querySelectorAll('div.gameboardCell');
-  gameboardCells.forEach(cell => cell.addEventListener('click', (e) => {
-  gameboard.setPiece(cell.getAttribute('id'));  
- }))};
+  gameboardCells.forEach(cell => cell.addEventListener('click', _clickHandler));
+ };
+
+ const _clickHandler = function(event) {
+  gameboard.setPiece(event.target.getAttribute('id'));
+  event.target
+
+ }
+ //remove event listener when game is over
+ const deactivateGameboard = () => {
+  let gameboardCells = document.querySelectorAll('div.gameboardCell');
+  gameboardCells.forEach(cell => cell.removeEventListener('click', _clickHandler)
+  );
+ };
+
+ const displayWinner = (num) => {
+  if (num == 0) {
+    const tieMessage = document.querySelector('#tie');
+    tieMessage.style.visibility = "visible";
+  } else if (num == 1) {
+    const p1Message = document.querySelector('#p1');
+    p1Message.style.visibility = "visible";
+  } else {
+    const p2Message = document.querySelector('#p2');
+    p2Message.style.visibility = "visible";
+  };
+ };
+
+ const _hideMessages = () => {
+    const hideMessage = document.querySelectorAll('.hide');
+    hideMessage.forEach(message => message.style.visibility = "");
+
+    const instructionMessage = document.querySelector('.instructions');
+    instructionMessage.style.visibility = "hidden";
+ };
 
  startGame();
 
  return {activateGameboard,
          startGame,
+         deactivateGameboard,
+         togglePlayerType,
+         displayWinner
         }
 
 })();
 
-//factories
-const Player = (playerType) => {
 
-};
 
